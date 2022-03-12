c82fc8354594ef653577f1bba8b3588e77dfb9c1
heyharshjaiswal <harshjaiswal2307@gmail.com>
2022-03-12 13:56:32 +0530

    minor-changes

--- BuildMyResume/settings.py
+++ BuildMyResume/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-from env import DEBUG, SECRET_KEY, ALLOWED_HOSTS
+from .env import DEBUG, SECRET_KEY, ALLOWED_HOSTS
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'BuildMyResume.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.posrgresql_psycogy2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'build_my_resume_db',
         'USER': 'postgres',
         'PASSWORD':'9405',
         'HOST':'127.0.0.1',
-        'POST':'5432',
+        'PORT':'5432',
     }
 }
 
