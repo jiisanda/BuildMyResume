35462664b31209e1da562c0f5682fdd2555d878a
heyharshjaiswal <harshjaiswal2307@gmail.com>
2022-03-15 00:52:06 +0530

    db

--- BuildMyResume/settings.py
+++ BuildMyResume/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'users',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -63,12 +64,8 @@ WSGI_APPLICATION = 'BuildMyResume.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'build_my_resume_db',
-        'USER': 'postgres',
-        'PASSWORD':'9405',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
