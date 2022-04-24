6aeb992e99615ad668609945ffb093473b77eba7
heyharshjaiswal <harshjaiswal2307@gmail.com>
2022-04-24 12:26:43 +0530

    adding-templates

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('my-profile/', views.profileTabView, name='profile'),
+    path('my-blog/', views.blogTabView, name='blog'),
+    path('contact-me/', views.contactTabView, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'home/home.html')
 
+def profileTabView(request):
+    return render(request, 'home/portfolio.html')
+
+def blogTabView(request):
+    return render(request, 'home/blog.html')
+
+def contactTabView(request):
+    return render(request, 'home/contact.html')
