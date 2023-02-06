47fef8328bdf4c10e8821e3a4d9950086279691d
harshjaiswal <harshjaiswal2307@gmail.com>
2023-02-06 17:16:24 +0530

    HomeProfileForm and UserProfileForm

--- home/forms.py
+++ home/forms.py
@@ -3,6 +3,41 @@ from django import forms
 from users.models import NewUser
 from .models import Profile, Certificates, Project
 
+
+
+class HomeProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['firstname', 'lastname', 
+                'country', 'state', 'city', 
+                'bio', 'role', 'phonenumber']
+
+        widgets = {
+            'firstname':forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name*'}),
+            'lastname':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name*'}),
+            
+            'country':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Country'}),
+            'state':forms.TextInput(attrs={'class':'form-control', 'placeholder':'State'}),
+            'city':forms.TextInput(attrs={'class':'form-control', 'placeholder':'City'}),
+            
+            'bio':forms.Textarea(attrs={'class':'form-control', 'placeholder':'Tell me about yourself.', 'rows':4}),
+            # 'profile_picture':forms.ImageField(),
+            'role':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Role'}),
+            'phonenumber':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Mobile No.'}),
+        }
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = NewUser
+        fields = ['email', 'username']
+        
+        # widgets = {
+        #     'email':forms.EmailField(max_length=254),
+        #     'username':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}),
+        # }
+
+
 class CertificateForm(forms.ModelForm):
     class Meta:
         model = Certificates
@@ -16,6 +51,7 @@ class CertificateForm(forms.ModelForm):
             'certificate_id':forms.TextInput(attrs={'class':'form-control','placeholder':'Certificate ID'}),
             'certificate_url':forms.URLInput(attrs={'class':'form-control', 'placeholder':'Link to drive/site'}),
         }
+
 
 class ProjectForm(forms.ModelForm):
     class Meta:
