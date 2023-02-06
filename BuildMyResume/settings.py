f293f6acfe936c0946e0f31c5c7830e01eb96edc
harshjaiswal <harshjaiswal2307@gmail.com>
2023-02-06 15:53:17 +0530

    update-settings.py

--- BuildMyResume/settings.py
+++ BuildMyResume/settings.py
@@ -1,18 +1,20 @@
 import os
-from .env import DEBUG, SECRET_KEY, ALLOWED_HOSTS
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = DEBUG
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ALLOWED_HOSTS
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(' ')
 
 
 # Application definition
