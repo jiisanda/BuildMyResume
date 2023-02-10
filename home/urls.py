fc17768c75f65394174c0bfa023104586479ab7d
harshjaiswal <harshjaiswal2307@gmail.com>
2023-02-10 12:21:57 +0530

    Populate saved data, update edit_home.html with single form

--- home/urls.py
+++ home/urls.py
@@ -5,9 +5,9 @@ from .views import index, profile_tab_view, blog_tab_view, contact_tab_view, edi
 app_name = 'home'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('<str:username>/', index, name='index'),
     path('my-profile/', profile_tab_view, name='profile'),
     path('my-blog/', blog_tab_view, name='blog'),
     path('contact-me/', contact_tab_view, name='contact'),
-    path('edit-home-page/', edit_home_page_view, name="edit_home")
+    path('edit-home-page/<str:username>/', edit_home_page_view, name="edit_home")
 ]
--- home/views.py
+++ home/views.py
@@ -1,14 +1,24 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from .forms import HomeProfileForm, UserProfileForm
+from .models import Profile
+
+from users.models import NewUser
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'home/home.html')
+@login_required
+def index(request, username):
+    context = {}
+    try:
+        uname = NewUser.objects.get(username=username)
+    except:
+        uname = None
+    context['user'] = uname
+    return render(request, 'home/home.html', context)
 
 @login_required
 def profile_tab_view(request):
@@ -24,13 +34,19 @@ def contact_tab_view(request):
 
 
 @login_required
-def edit_home_page_view(request):
+def edit_home_page_view(request, username):
+    requested_user = NewUser.objects.get(username=username)
+    requested_user_profile = Profile.objects.get(id=requested_user.id)
     if request.method == 'POST':
         # below two lines will bring all the save data of the user...
-        home_profile_form = HomeProfileForm(request.POST, request.FILES, instance=request.user)
-        user_profile_form = UserProfileForm(request.POST, 
+        # request_user = get_object_or_404(Profile, id=id)
+        home_profile_form = HomeProfileForm(request.POST, 
                                             request.FILES, 
-                                            instance=request.user.profile)
+                                            instance=requested_user
+                                        )
+        user_profile_form = UserProfileForm(request.POST,
+                                            instance=request.user.profile
+                                        )
 
         if home_profile_form.is_valid() and user_profile_form.is_valid():
             home_profile_form.save()
@@ -44,6 +60,8 @@ def edit_home_page_view(request):
     context = {
         'home_profile_form':home_profile_form,
         'user_profile_form':user_profile_form,
+        'requested_user':requested_user,
+        'requested_user_profile':requested_user_profile,
     }
 
     return render(request, 'home/edit_home.html', context)
