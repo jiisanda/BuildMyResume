630c5ed7a11c00b99f38ff9e61cc6256f5913907
harshjaiswal <harshjaiswal2307@gmail.com>
2023-02-19 02:34:29 +0530

    restructure-usersapp(unstable)

--- BuildMyResume/urls.py
+++ BuildMyResume/urls.py
@@ -3,15 +3,27 @@
 The `urlpatterns` list routes URLs to views. 
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from users import views as users_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('accounts/', include('users.urls')),
-    path('', include('home.urls')),
+    path('accounts/', include('allauth.urls')),
+
+    path('signup/', users_views.signup, name='signup'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_rest.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('activate/<uidb64>/<token>/', users_views.activate, name='activate'),
 
     path('__debug__/', include('debug_toolbar.urls')),
 
