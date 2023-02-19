323de5484eae21ea7c2ce74455c62153c36f786a
harshjaiswal <harshjaiswal2307@gmail.com>
2023-02-19 13:46:46 +0530

    ＞︿＜

--- BuildMyResume/urls.py
+++ BuildMyResume/urls.py
@@ -16,14 +16,14 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 
     path('signup/', users_views.signup, name='signup'),
+    path('activate/<uidb64>/<token>/', users_views.activate, name='activate'),
 
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login'), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_rest.html'), name='password_reset'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-    path('activate/<uidb64>/<token>/', users_views.activate, name='activate'),
 
     path('__debug__/', include('debug_toolbar.urls')),
 
