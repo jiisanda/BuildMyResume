8894903a03fbba37ee91edbcab27b0ccc512ce76
heyharshjaiswal <harshjaiswal2307@gmail.com>
2023-03-09 17:46:27 +0530

    STATICFILES_DIRS settings

--- BuildMyResume/settings.py
+++ BuildMyResume/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_DIRS = [os.path.join(BASE_DIR, 'home/static')]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "home/static",
+# ]
 
 
 # Media Files
