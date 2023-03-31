73a60722d42772feeab4f2ba8d1ff63650ad1d11
heyharshjaiswal <harshjaiswal2307@gmail.com>
2023-03-31 18:42:47 +0530

    simple option

--- home/views.py
+++ home/views.py
@@ -54,82 +54,100 @@ def choose(request, pk):
     
     if request.method == 'GET':
         form = ChooseForm()
+    
     elif request.method == 'POST' and 'view-resume' in request.POST:
         if form.is_valid() and form.cleaned_data['resume_template'] == 'default':
             return render(request, 'home/default.html', {'form': form, 'resume':resume, 'profile_picture_url':profile_picture_url})
+        
+        elif form.is_valid() and form.cleaned_data['resume_template'] == 'simple':
+            return render(request, 'home/simple.html', {'form': form, 'resume':resume, 'profile_picture_url':profile_picture_url})
+    
     elif form.is_valid() and request.method == 'POST' and 'export-resume' in request.POST:
+        
         if form.cleaned_data['resume_template'] == 'default':
             template_path = 'home/pdf_default.html'
-            context = {'form': form, 'resume':resume, 'profile_picture_url':profile_picture_url}
-            response = HttpResponse(content_type='application/pdf')
-            response['Content-Disposition'] = 'filename="my_resume.pdf"'
-            # find the template and render it.
-            template = get_template(template_path)
-            html = template.render(context)
-
-            # create a pdf
-            pisa_status = pisa.CreatePDF(
-            html, dest=response)
-            # if error then show some funny view
-            if pisa_status.err:
-                return HttpResponse('We had some errors <pre>' + html + '</pre>')
-            return response
+        
+        elif form.cleaned_data['resume_template'] == 'simple':
+            template_path = 'home/pdf_simple.html'
+            
+        context = {'form': form, 'resume':resume, 'profile_picture_url':profile_picture_url}
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'filename="my_resume.pdf"'
+        
+        # find the template and render it.
+        template = get_template(template_path)
+        html = template.render(context)
+        
+        # create a pdf
+        pisa_status = pisa.CreatePDF(
+        html, dest=response)
+        
+        # if error then show some view
+        if pisa_status.err:
+            return HttpResponse(f'We had some errors <pre>{html}</pre>')
+        return response
+    
     return render(request, 'home/choose.html', {'form':form, 'resume':resume})
 
 
 @login_required()
 def my_resume(request):
+    
     user = request.user
     resumes = ResumeMetaData.objects.filter(user=user).order_by('-created_at')
+    
     return render(request, 'home/my_resumes.html', {'resumes': resumes})
 
 @login_required()
 def templates(request):
+    
     return render(request, 'home/templates.html')
 
 @login_required()
 def edit_profile(request):
+    
     if request.method == 'POST':
         user_form = CustomUserChangeForm(request.POST, instance=request.user)
         profile_form = ProfileUpdateFrom(request.POST, request.FILES, instance=request.user.profile)
+        
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
             messages.success(request, "Profile has been saved!")
+            
             return HttpResponseRedirect(reverse('home:edit-profile'))
+    
     else:
         user_form = CustomUserChangeForm(instance=request.user)
         profile_form = ProfileUpdateFrom(instance=request.user.profile)
+    
     context = {
         'user_form':user_form,
         'profile_form':profile_form,
     }
+    
     return render(request, 'home/profile-edit.html', context)
 
 
 @login_required()
 def delete_my_resume(request, pk):
+    
     resume = ResumeMetaData.objects.get(pk=pk)
     resume.delete()
     messages.success(request, "Resume is successfully deleted!")
+    
     return HttpResponseRedirect(reverse('home:my-resume'))
 
 def dict_has_data(input_dict):
-    is_data_present = False
-    for key in input_dict:
-        if input_dict[key]:
-            is_data_present = True
-            break
-    return is_data_present
+    
+    return any(input_dict[key] for key in input_dict)
 
 
 class ResumeBucket(LoginRequiredMixin, SessionWizardView):
     login_url = '/login/'
     
     def get_form_initial(self, step):
-        if 'pk' in self.kwargs:
-            return {}
-        return self.initial_dict.get(step, {})
+        return {} if 'pk' in self.kwargs else self.initial_dict.get(step, {})
     
     def get_form_instance(self, step):
         if 'pk' in self.kwargs:
@@ -137,52 +155,67 @@ class ResumeBucket(LoginRequiredMixin, SessionWizardView):
             resume = ResumeMetaData.objects.get(id=pk)
             if step == 'resumes':
                 return resume
+            
             if step == 'education':
                 return resume.education_set.all()
+            
             if step == 'experience':
                 return resume.experience_set.all()
+            
             if step == 'certificates':
                 return resume.certificate_set.all()
+            
             if step == 'skills':
                 return resume.skill_set.all()
+            
             if step == 'languages':
                 return resume.language_set.all()
+            
             if step == 'courseworks':
                 return resume.coursework_set.all()
+            
             if step == 'projects':
                 return resume.project_set.all()
+        
         else:
             if step == 'resumes':
                 return None
+            
             if step == 'education':
                 return Education.objects.none()
+            
             if step == 'experience':
                 return Experience.objects.none()
+            
             if step == 'certificates':
                 return Certificate.objects.none()
+            
             if step == 'skills':
                 return Skill.objects.none()
+            
             if step == 'languages':
                 return Language.objects.none()
+            
             if step == 'courseworks':
                 return Coursework.objects.none()
+            
             if step == 'projects':
                 return Project.objects.none()
         return None
     
     def get_template_names(self):
+        
         return [TEMPLATES[self.steps.current]]
     
     def done(self, form_list, **kwargs):
         user = self.request.user
         resume_form_data = self.get_cleaned_data_for_step('resumes')
         resume_name = resume_form_data['resume_name']
-        if 'pk' in self.kwargs:
-            pk = self.kwargs['pk']
-        else:
-            pk = None
+        
+        pk = self.kwargs['pk'] if 'pk' in self.kwargs else None
+        
         resume, created = ResumeMetaData.objects.update_or_create(id=pk, defaults={'user':user, 
-                                                                                   'resume_name':resume_name, })
+                                                                                'resume_name':resume_name, })
         
         for form_name in FORM_TYPES:
             form_data_list = self.get_cleaned_data_for_step(form_name)
@@ -192,11 +225,12 @@ class ResumeBucket(LoginRequiredMixin, SessionWizardView):
                 form_data['resume'] = resume
                 
                 form_instance = self.get_form(step=form_name)
-                obj = form_data.pop('id')
-                if obj:
+                
+                if obj := form_data.pop('id'):
                     form_instance.model.objects.filter(id=obj.id).update(**form_data)
                 else:
                     form_instance.model.objects.create(**form_data)
         
         messages.add_message(self.request, messages.SUCCESS, message="Resume Saved!")
+        
         return HttpResponseRedirect(reverse('home:my-resume'))
